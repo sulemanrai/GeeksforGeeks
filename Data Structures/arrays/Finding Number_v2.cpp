0b66f3b936e10a7463725536dc36f96e39fcd9ad
sulemanrai <suleman.rai@gmail.com>
2018-06-03 22:17:39 +0800

    Finding Number v2

--- Data Structures/arrays/Finding Number_v2.cpp
+++ Data Structures/arrays/Finding Number_v2.cpp
@@ -54,19 +54,45 @@ int binarySearch(int arr[], int l, int r, int x)
 	// present in array
 	return -1;
 }
+int rightbinarysearcy(int arr[], int l, int r, int x)
+{
+	if (r >= l)
+	{
+		int mid = l + (r - l) / 2;
+
+		// If the element is present at the middle
+		// itself
+		if (arr[mid] == x)
+			return mid;
+
+		// If element is smaller than mid, then
+		// it can only be present in left subarray
+		if (arr[mid] < x)
+			return rightbinarysearcy(arr, l, mid - 1, x);
+
+		// Else the element can only be present
+		// in right subarray
+		return rightbinarysearcy(arr, mid + 1, r, x);
+	}
+
+	// We reach here when element is not
+	// present in array
+	return -1;
+}
 int printIndex(int arr[], int n, int key)
 {
 	int max = findMaximum(arr, 0, n - 1);
 	int index = -1;
 	if (key == arr[max])
 		return max;
-	if (key < arr[max] && key >= arr[0])
-		return binarySearch(arr, 0, max, key);
-
-	if (key < arr[max] && key < arr[0])
-		return binarySearch(arr, max,n-1, key);
-
-	return -1;
+	if (key < arr[max]) {
+		index = binarySearch(arr, 0, max, key);
+	}
+	if(index == -1)
+	{
+		index = rightbinarysearcy(arr, max+1, n - 1, key);
+	}
+	return index;
 }
 int main()
 {
